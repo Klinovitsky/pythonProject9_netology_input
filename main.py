90ef6db596ff636860dc33b945430d6904aca70f
Klinovitsky <klinovitsky@gmail.com>
2023-02-19 00:16:40 +0300

    Ver 2

--- main.py
+++ main.py
@@ -1,5 +1,22 @@
 # Netology: input
 
 name = input("Type your name: ")
-print(name)
+print("Привет", name)
+
+
+# Сначала нужно импортировать
+# класс datetime из модуля datetime, после чего создать объект datetime. Модуль предоставляет метод now()
+
+import datetime
+
+dt = datetime.datetime.now()
+print (dt)
+
+
+
+sum = 0
+array = [45, 7, -934, 0, 2839]
+for i in array:
+   sum += i
+print(sum)
 
