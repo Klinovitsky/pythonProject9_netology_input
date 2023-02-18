240f13065fe74741acee48e37019d81d2c9bacdd
Klinovitsky <klinovitsky@gmail.com>
2023-02-19 00:57:14 +0300

    Ver 3

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
 # Netology: input
 
-name = input("Type your name: ")
-print("Привет", name)
-
+# name = input("Type your name: ")
+# print("Привет", name)
 
 # Сначала нужно импортировать
 # класс datetime из модуля datetime, после чего создать объект datetime. Модуль предоставляет метод now()
@@ -10,13 +9,36 @@ print("Привет", name)
 import datetime
 
 dt = datetime.datetime.now()
-print (dt)
+current_time = dt.time()
+# onlyd = datetime.datetime.date()
+dt_now = datetime.datetime.utcnow()
 
+attr = dir(datetime)
+print(attr)
 
+print ("dt: ", dt, "time: ", current_time, "UTC: ", dt_now)
+
+date_string = "02/19/23"
+date_obj = datetime.datetime.strptime(date_string, '%m/%d/%y')
+
+print(date_obj)
+
+print(type(5))
+print(type(5.8))
 
 sum = 0
-array = [45, 7, -934, 0, 2839]
+array = [45000, 7, -934, 0, 2839]
 for i in array:
    sum += i
 print(sum)
 
+print(17/2) #обычное деление - результат 8.5
+print(17//2) #целочисленное деление - результат 8, целая часть при делении
+print(17%2) #деление с остатком - результат 1, остаток при делении
+
+str1 = 'Hello, '
+str2 = 'world!'
+print(str1+str2)
+
+a = None
+print(type(a))
